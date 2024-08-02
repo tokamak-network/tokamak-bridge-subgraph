f955d8e1e063c1ea368317e58ed8c4641550f88e
SonYoungsung <sysgigigi@gmail.com>
2024-08-02 11:19:16 +0900

    Initialize subgraph

---
+++ tokamak-bridge-ct-titan:sepolia/tests/l-2-cross-trade.test.ts
@@ -0,0 +1,74 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
+import { CancelCT } from "../generated/schema"
+import { CancelCT as CancelCTEvent } from "../generated/L2CrossTrade/L2CrossTrade"
+import { handleCancelCT } from "../src/l-2-cross-trade"
+import { createCancelCTEvent } from "./l-2-cross-trade-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let _requester = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let _totalAmount = BigInt.fromI32(234)
+    let _saleCount = BigInt.fromI32(234)
+    let _l2chainId = BigInt.fromI32(234)
+    let newCancelCTEvent = createCancelCTEvent(
+      _requester,
+      _totalAmount,
+      _saleCount,
+      _l2chainId
+    )
+    handleCancelCT(newCancelCTEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("CancelCT created and stored", () => {
+    assert.entityCount("CancelCT", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "CancelCT",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "_requester",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "CancelCT",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "_totalAmount",
+      "234"
+    )
+    assert.fieldEquals(
+      "CancelCT",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "_saleCount",
+      "234"
+    )
+    assert.fieldEquals(
+      "CancelCT",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "_l2chainId",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
